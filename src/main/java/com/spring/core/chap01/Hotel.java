1117d7f5db7f6ac07d86be2cef1f1c8775c47de9
Kanggwanju <xprtktm1@gmail.com>
2025-07-17 12:42:08 +0900

    호텔 관리 및 레스토랑 구성 로직 개선, DI 적용, 제어의 역전 및 새로운 코스(이탈리안) 추가

--- src/main/java/com/spring/core/chap01/Hotel.java
+++ src/main/java/com/spring/core/chap01/Hotel.java
@@ -7,20 +7,27 @@ public class Hotel {
     private Restaurant restaurant;
 
     // 헤드쉐프 고용
-//    private JannChef headChef;
+//    private JannChef headChef; // DIP 위반
     private Chef headChef;
 
     // 생성자
 
-    public Hotel() {
-        this.restaurant = new WesternRestaurant();
+    public Hotel(Restaurant restaurant, Chef chef) {
+        // OCP 위반, 셰프만 변경되어도 레스토랑,
+        // 레스토랑 내부의 헤드셰프를 전부 변경해야함(코드 수정).
+//        this.restaurant = new WesternRestaurant();
 //        this.headChef = new StephaneChef(); // DIP 위반
-        this.headChef = new StephaneChef();
+//        this.headChef = new JannChef();
+
+        // 제어의 역전, 객체 생성의 제어권을 넘김.
+        this.restaurant = restaurant;
+        this.headChef = chef;
     }
 
     // 레스토랑 예약 기능
     public void reserve() {
         System.out.println("레스토랑을 예약합니다.");
+        System.out.println("헤드 셰프명: " + headChef.getClass().getSimpleName());
         restaurant.order();
     }
 
